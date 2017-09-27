d2ab0733d445326f91e30500873dc8316ec7f06d
essejose <ze_pk2@hotmail.com>
2017-09-27 16:25:33 -0300

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,28 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EscolhaPage } from '../pages/escolha/escolha';
+import { HttpModule } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EscolhaPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EscolhaPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit  } from '@angular/core';
+import {
+  NavController,
+  LoadingController,
+  AlertController
+} from 'ionic-angular';
+import { Http } from '@angular/http';
+
+import {EscolhaPage} from '../escolha/escolha'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit  {
 
-  constructor(public navCtrl: NavController) {
+  public carros;
 
-  }
+  constructor(
+    public navCtrl: NavController,
+    private _http: Http,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController) { }
+
+
+    ngOnInit() {
+    let loader = this._loadingCtrl.create({
+      content: 'Buscando novos carros. Aguarde ...'
+    });
+
+
+    loader.present();
+
+    this._http
+      .get('http://www.mocky.io/v2/59cbf8a8260000a80f6b76c1')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => {
+        this.carros = carros
+        loader.dismiss();
+      }, err => {
+        console.log(err);
+
+        loader.dismiss();
+
+        this._alertCtrl
+          .create({
+            title: 'Falha na conexão',
+            message: 'error',
+            buttons: [{ text: 'estou ciente' }],
+            subTitle: 'Deu nao'
+          }).present();
+
+      });
+  
+  
+    }
+
+    seleciona(carro) {
+       
+          this.navCtrl.push(EscolhaPage,{carroSelecionado:carro})
+    }
 
 }
